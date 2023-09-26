ea9ea06aa05e90d78f55c687a1b732569e2bde0e
gtadini <gabrieltadini@hotmail.com>
2023-09-25 21:07:46 -0300

    conexion con la bd

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
 export function GET() {
+    return NextResponse.json("obteniendo  tareas")
+}
+
+export function POST() {
     return NextResponse.json("creando tareas")
 }
