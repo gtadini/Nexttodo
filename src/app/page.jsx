a09ea56c164dc4b60db4678c419d32aa960d4d43
gtadini <gabrieltadini@hotmail.com>
2023-09-28 20:07:09 -0300

    cree mi primer componente

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,30 @@
-function HomePage() {
+import {prisma} from '@/libs/prisma'
+import TaskCard from '@/components/TaskCard';
+
+async function loadTask() {
+  //obteniendo de la base de datos (en este enfoque consulto la base de datos)
+  //const res = await fetch('http://localhost:3000/api/tasks')
+  //const data = await res.js on()
+  //console.log(data) 
+  
+  //haciendo una petición HTTP api/tasks (en este enfoque me comunico con mi propio backend)
+  return await prisma.task.findMany()
+  
+}
+
+async function HomePage() {
+  
+  const tasks = await loadTask();
+  console.log(tasks);
+  
   return(
-    <div>Tareas</div>
+    <section className='container mx-auto'>
+      <div className='grid grid-cols-3 gap-3 mt-10'>
+      {tasks.map((task) => (
+        <TaskCard task={task} key={task.id} /> 
+      ))}
+    </div>
+    </section>
   )
 }
 export default HomePage;
