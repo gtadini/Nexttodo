8bad73741e84d65dff4aa1c316d58c9c16d45e3b
gtadini <gabrieltadini@hotmail.com>
2023-10-06 00:40:35 -0300

    solucion error build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
     return(
         <nav className='bg-slate-900'>
             <div className='container mx-auto flex justify-between items-center py-3'>
-            <Link href={'/'}><h3 className='text-slate-300 hover:text-slate-200'className='font-bold text-3xl'>
+            <Link href={'/'}><h3 className='text-slate-300 hover:text-slate-200 font-bold text-3xl'>
                 NextCRUD
             </h3></Link>
             <ul className='flex gap-x-2 text-lg font-bold'>
